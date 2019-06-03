dcb2ebb3e6d9fef64f9289ca032c68a5d7dfc9b1
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-03 17:06:28 -0400

    Completed lab

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
